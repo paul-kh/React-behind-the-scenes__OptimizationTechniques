eb6ca594eff4a251a15ce60c90aa96ef1c75c9e0
Paul Chheang <phalcheat@gmail.com>
2024-07-02 12:17:19 -0500

    Used Clever Component Structuring to avoid unnecessary component rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,31 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import Counter from './components/Counter/Counter.jsx';
-import Header from './components/Header.jsx';
-import { log } from './log.js';
+import Counter from "./components/Counter/Counter.jsx";
+import Header from "./components/Header.jsx";
+import ConfigureCounter from "./components/ConfigureCounter.jsx";
+import { log } from "./log.js";
 
 function App() {
-  log('<App /> rendered');
+  log("<App /> rendered");
 
-  const [enteredNumber, setEnteredNumber] = useState(0);
   const [chosenCount, setChosenCount] = useState(0);
 
-  function handleChange(event) {
-    setEnteredNumber(+event.target.value);
-  }
-
-  function handleSetClick() {
-    setChosenCount(enteredNumber);
-    setEnteredNumber(0);
+  function handleSetCount(newCount) {
+    setChosenCount(newCount);
   }
 
   return (
     <>
       <Header />
       <main>
-        <section id="configure-counter">
-          <h2>Set Counter</h2>
-          <input type="number" onChange={handleChange} value={enteredNumber} />
-          <button onClick={handleSetClick}>Set</button>
-        </section>
+        {/*************************************************************
+         * When we changed the counter on every key stroke,
+         * only <ConfigureCounter> component gets executed,
+         * not all components anymore. This saves us from
+         * using memo() to prevent unnecessary component rendering.
+         **************************************************************
+         */}
+        <ConfigureCounter onSetCount={handleSetCount} />
         <Counter initialCount={chosenCount} />
       </main>
     </>
